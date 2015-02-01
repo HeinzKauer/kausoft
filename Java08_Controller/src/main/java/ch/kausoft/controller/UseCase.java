597770ff9fe37f3d753f65e50681fed57de8f798
Heinz Kauer <heinz.kauer@gmx.net>
2015-02-01 22:02:07 +0100

    UseCase bereinigt

--- Java08_Controller/src/main/java/ch/kausoft/controller/UseCase.java
+++ Java08_Controller/src/main/java/ch/kausoft/controller/UseCase.java
@@ -3,13 +3,9 @@
  */
 package ch.kausoft.controller;
 
-import java.util.Map;
-
-import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 import ch.kausoft.context.Context;
-import ch.kausoft.context.SessionContext;
 
 /**
  * Ein UseCase ist gleich wie ein Anwendungsfall.
@@ -30,7 +26,9 @@ public abstract class UseCase {
 	private String useCase_ID;
 
 	/**
-	 * In diesen Conext wurde das UseCase registriert.
+	 * In diesen Context wurde das zulezt UseCase registriert. Wird ein
+	 * UseCase in einen Context gespeichert, registriert sich der
+	 * Context hier im UseCase
 	 */
 	@Getter
 	@Setter
@@ -42,27 +40,5 @@ public abstract class UseCase {
 	protected UseCase(String id) {
 		useCase_ID = id;
 	}
-
-
-
-//	/**
-//	 * @since created at 31.01.2015.00:45:42
-//	 * @param context
-//	 * @param id
-//	 * @return UseCase
-//	 */
-//	public static UseCase loadInstance(Context context, String id) {
-//		return (UseCase) context.getUseCase().get(id);
-//	}
-//
-//	/**
-//	 * @since created at 31.01.2015.00:45:45
-//	 * @param pUseCase_ID
-//	 *          void
-//	 */
-//	private void setUsseCase(String pUseCase_ID) {
-//		Map<String, UseCase> actionResult = getContext().getUseCase();
-//		actionResult.put(pUseCase_ID, this);
-//	}
 
 }
