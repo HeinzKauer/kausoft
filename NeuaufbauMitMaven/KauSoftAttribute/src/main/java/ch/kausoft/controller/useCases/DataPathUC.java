df7ffa42876919a5758a83683a87ae05d06a5053
Heinz Kauer <heinz.kauer@gmx.net>
2017-11-03 23:28:25 +0100

    Save Zusammenführnung alles in ein Maven Projekt
    Signed-off-by: Heinz Kauer <heinz.kauer@gmx.net>

--- NeuaufbauMitMaven/KauSoftAttribute/src/main/java/ch/kausoft/controller/useCases/DataPathUC.java
+++ NeuaufbauMitMaven/KauSoftAttribute/src/main/java/ch/kausoft/controller/useCases/DataPathUC.java
@@ -1,58 +1,55 @@
 /**
- * 
+ *
  */
 package ch.kausoft.controller.useCases;
 
-import lombok.Getter;
-import lombok.Setter;
 import ch.kausoft.context.SessionContext;
 import ch.kausoft.controller.UseCase;
+import lombok.Data;
+import lombok.ToString;
 
 /**
  * @author Heinz
  */
+@ToString
+@Data
 public class DataPathUC extends UseCase {
 
-	/**
-	 * 
-	 */
-	@Getter
-	@Setter
-	String dataPath = "/";
+    /**
+     *
+     */
+    String dataPath = "/";
 
-	/**
-	 * 
-	 */
-	@Getter
-	@Setter
-	String dataName = "readme";
+    /**
+     *
+     */
+    String dataName = "readme";
 
-	/**
-	 * 
-	 */
-	@Getter
-	@Setter
-	String dataType = "txt";
+    /**
+     *
+     */
+    String dataType = "txt";
 
-	/**
-	 * @param id
-	 */
-	public DataPathUC(String id) {
-		super(id);
-	}
+    /**
+     * @param id
+     */
+    public DataPathUC(String id) {
+        super(id);
+    }
 
-	/**
-	 * Test Main
-	 * 
-	 * @since created at 30.01.2015.22:13:36
-	 * @param args
-	 *          void
-	 */
-	public static void main(String[] args) {
+    /**
+     * Test Main
+     *
+     * @param args void
+     * @since created at 30.01.2015.22:13:36
+     */
+    public static void main(String[] args) {
+        DataPathUC uc = new DataPathUC("4711");
+        SessionContext.getContext().add(uc);
+        System.out.println("uc = " + uc.toString());
+        uc.setDataPath("sss\\ssssssssssss");
+        System.out.println("uc = " + uc.toString() +" path" +uc.getClass().getPackage());
 
-		DataPathUC uc = new DataPathUC("4711");
-		SessionContext.getContext().add(uc);
-		System.out.println(uc.toString());
-	}
+    }
 
 }
